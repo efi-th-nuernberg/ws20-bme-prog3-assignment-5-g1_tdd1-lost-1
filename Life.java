0bc7a673efe81729ad91ee271d0788489078d990
repl.it user <replituser@example.com>
2020-11-13 15:58:10 +0000

    Assignment 5

--- Life.java
+++ Life.java
@@ -9,13 +9,13 @@ public class Life implements ILife {
     l = (Life) l.nextGeneration();
   }
 
-
+  //Konstruktor
   public Life() {
     nukeAll();
+    this.l(int x, int y);
   }
 
   public Life(String[] setup) {
-    this();
     for (int y = 0; y < setup.length; y++)
       for (int x = 0; x < setup[y].length(); x++)
         if (setup[y].charAt(x) != ' ')
@@ -32,6 +32,41 @@ public class Life implements ILife {
   @Override
   public void setAlive(int x, int y) {
     // TODO Auto-generated method stub
+    this.Life(x,y);
+      int count = 0;
+
+    if ((x-1, y+1)  != ' '){
+        return true; 
+    } else {
+        return false;
+    }
+
+    ((x-1, y)  != ' '){
+        return true; 
+    }
+
+    ((x-1, y-1)  != ' '){
+        return true; 
+    }
+    
+    ((x, y+1)  != ' '){
+        return true; 
+    }
+
+    ((x, y-1)  != ' '){
+        return true; 
+    }
+    
+    ((x+1, y+1)  != ' '){
+        return true; 
+    }
+
+     ((x+1, y)  != ' '){
+        return true; 
+    }
+     ((x+1, y-1)  != ' '){
+        return true; 
+    }
 
   }
 
@@ -44,12 +79,20 @@ public class Life implements ILife {
   @Override
   public boolean isAlive(int x, int y) {
     // TODO Auto-generated method stub
-    return false;
+     for (int y = 0; y < setup.length(); y++)
+      for (int x = 0; x < setup[y].length(); x++)
+        if (setup[y].charAt(x) != ' ')
+            return true;
+      }
+    }
+   
+    
   }
 
   @Override
   public ILife nextGeneration() {
     // TODO Auto-generated method stub
+
     return null;
   }
 }
